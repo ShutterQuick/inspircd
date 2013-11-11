c290da933e32ea83478cf8094d741b0e29094767
Daniel Vassdal <daniv09@student.uia.no>
2013-11-11 12:28:27 -0800

    Modified m_sajoin to be able to function more like Charybdis-type ircd's ojoin, allowing to specify wanted level Also added better control on who it can be used on.

--- src/modules/m_sajoin.cpp
+++ src/modules/m_sajoin.cpp
@@ -26,24 +26,100 @@
 class CommandSajoin : public Command
 {
  public:
-	CommandSajoin(Module* Creator) : Command(Creator,"SAJOIN", 2)
+	CommandSajoin(Module* Creator) : Command(Creator,"SAJOIN", 1)
 	{
 		allow_empty_last_param = false;
-		flags_needed = 'o'; Penalty = 0; syntax = "<nick> <channel>";
+		flags_needed = 'o'; Penalty = 0; syntax = "[<nick>] [prefix]<channel>";
 		TRANSLATE2(TR_NICK, TR_TEXT);
 	}
 
 	CmdResult Handle (const std::vector<std::string>& parameters, User *user)
 	{
-		User* dest = ServerInstance->FindNick(parameters[0]);
+		User* dest;
+		std::string channelstring;
+
+		if (parameters.size() > 1)
+		{
+			dest = ServerInstance->FindNick(parameters[0]);
+			channelstring = parameters[1];
+		}
+		else
+		{
+			dest = user;
+			channelstring = parameters[0];
+		}
+
 		if ((dest) && (dest->registered == REG_ALL))
 		{
+			if ((user == dest && !user->HasPrivPermission("users/sajoin/self", true)) ||
+				(user != dest && !user->HasPrivPermission("users/sajoin/others", true)))
+			{
+				user->WriteNotice("*** You are not allowed to use sajoin on the user you tried");
+				return CMD_FAILURE;
+			}
+
+			std::vector<PrefixMode*> pm;
+			size_t hashpos = channelstring.find('#');
+			std::string modestring = channelstring.substr(0, hashpos);
+			std::string channel = channelstring.substr(hashpos);
+			if (hashpos)
+			{
+				if (!user->HasPrivPermission("users/sajoin/modes", true))
+				{
+					user->WriteNotice("*** You are not allowed to specify modes for sajoin");
+					return CMD_FAILURE;
+				}
+
+				ModeHandler* mh;
+				std::string illegal_modes;
+				std::string oper_modes;
+				for (unsigned int i = 0; i < modestring.size(); i++)
+				{
+					if (illegal_modes.find(modestring[i]) != std::string::npos)
+						continue;
+
+					mh = ServerInstance->Modes->FindPrefix(modestring[i]);
+					PrefixMode* tpm;
+					if (mh)
+						tpm = mh->IsPrefixMode();
+
+					if (!mh || !tpm)
+					{
+						illegal_modes.push_back(modestring[i]);
+						continue;
+					}
+
+					if (tpm->NeedsOper() && !user->HasModePermission(tpm->GetModeChar(), MODETYPE_USER))
+					{
+						oper_modes.push_back(modestring[i]);
+						continue;
+					}
+
+					std::vector<PrefixMode*>::iterator it = std::find(pm.begin(), pm.end(), tpm);
+					if (it == pm.end())				
+						pm.push_back(tpm); 	
+				}
+
+				if (illegal_modes.size())
+				{
+					user->WriteNotice("*** The following modes are not valid prefix modes: " + illegal_modes);
+					return CMD_FAILURE;
+				}
+
+				if (oper_modes.size())
+				{
+					user->WriteNotice("*** The following prefix modes requires the target to be an oper: " + oper_modes);
+						return CMD_FAILURE;
+				}
+			}
+
+
 			if (ServerInstance->ULine(dest->server))
 			{
 				user->WriteNumeric(ERR_NOPRIVILEGES, "%s :Cannot use an SA command on a u-lined client",user->nick.c_str());
 				return CMD_FAILURE;
 			}
-			if (IS_LOCAL(user) && !ServerInstance->IsChannel(parameters[1]))
+			if (IS_LOCAL(user) && !ServerInstance->IsChannel(channel))
 			{
 				/* we didn't need to check this for each character ;) */
 				user->WriteNotice("*** Invalid characters in channel name or name too long");
@@ -57,29 +133,42 @@ class CommandSajoin : public Command
 			LocalUser* localuser = IS_LOCAL(dest);
 			if (localuser)
 			{
-				Channel* n = Channel::JoinUser(localuser, parameters[1], true);
+				Channel* n = Channel::JoinUser(localuser, channel, true);
 				if (n)
 				{
 					if (n->HasUser(dest))
 					{
-						ServerInstance->SNO->WriteToSnoMask('a', user->nick+" used SAJOIN to make "+dest->nick+" join "+parameters[1]);
+						ServerInstance->SNO->WriteToSnoMask('a', user->nick+" used SAJOIN to make "+dest->nick+" join " + modestring + channel);
+
+						irc::modestacker modestack(true);
+						for (std::vector<PrefixMode*>::iterator it = pm.begin(); it != pm.end(); it++)
+							modestack.Push((*it)->GetModeChar(), dest->nick);
+
+						parameterlist stackresult;
+						stackresult.push_back(n->name);
+						while (modestack.GetStackedLine(stackresult))
+						{
+							ServerInstance->Modes->Process(stackresult, ServerInstance->FakeClient);
+							stackresult.erase(stackresult.begin() + 1, stackresult.end());
+						}
+
 						return CMD_SUCCESS;
 					}
 					else
 					{
-						user->WriteNotice("*** Could not join "+dest->nick+" to "+parameters[1]+" (User is probably banned, or blocking modes)");
+						user->WriteNotice("*** Could not join "+dest->nick+" to "+channel+" (User is probably banned, or blocking modes)");
 						return CMD_FAILURE;
 					}
 				}
 				else
 				{
-					user->WriteNotice("*** Could not join "+dest->nick+" to "+parameters[1]);
+					user->WriteNotice("*** Could not join "+dest->nick+" to "+channel);
 					return CMD_FAILURE;
 				}
 			}
 			else
 			{
-				ServerInstance->SNO->WriteToSnoMask('a', user->nick+" sent remote SAJOIN to make "+dest->nick+" join "+parameters[1]);
+				ServerInstance->SNO->WriteToSnoMask('a', user->nick+" sent remote SAJOIN to make "+dest->nick+" join "+channel);
 				return CMD_SUCCESS;
 			}
 		}
